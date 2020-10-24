4cefa6aee2e32fe1fd56666917fd13079426c93f
1ndianpanda <adityapandya2013@gmail.com>
2020-10-24 14:53:47 -0400

    Twilio

--- twilio.py
+++
@@ -1,7 +0,0 @@
-from twilio.rest import Client
-
-account_sid = 'AC46368bace24b8dd3f7290ecca8d6e664'
-auth_token = 'ab08351b6999fc8f3123da18e1b60332'
-client = Client(account_sid, auth_token)
-
-message = client.messages.create(body="Your house is on fire.", from_='+12513151614', to='+17166501231')
