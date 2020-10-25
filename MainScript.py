9054f9edc2fe12836430b4d408001ec375717dfd
Budlu <48537618+Budlu@users.noreply.github.com>
2020-10-24 23:32:02 -0400

    Update MainScript.py

--- MainScript.py
+++ MainScript.py
@@ -3,16 +3,16 @@ import time
 
 from twilio.rest import Client
 
-def send_text(sendTo,body):
+def send_text(sendTo):
     account_sid = 'AC46368bace24b8dd3f7290ecca8d6e664'
     auth_token = 'ab08351b6999fc8f3123da18e1b60332'
     client = Client(account_sid, auth_token)
 
-    message = client.messages.create(body=body, from_='+12513151614', to=sendTo)
-
+    message = client.messages.create(body="Your house is on fire.", from_='+12513151614', to=sendTo)
+    
 def arduino_activated(pn):
     send_text(pn)
-
+    
 BAUD_RATE = 9600
 PORT = "COM3"
 BYTESIZE = 8
@@ -24,8 +24,11 @@ serialPort = serial.Serial(port=PORT, baudrate=BAUD_RATE, timeout=TIMEOUT)
 
 while(True):
     data = serialPort.read(1)
-    character = data.decoce("ASCII")
+    character = data.decode("ASCII")
+    print(character)
     if (character == 'b'):
         arduino_activated(PHONE_NUMBER)
         break
     time.sleep(1)
+    
+serialPort.close()
